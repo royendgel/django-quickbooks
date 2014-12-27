8a0ba012fa7d75705cd31f38eda7804064ea2d66
Royendgel Silberie <royendgel@gmail.com>
2014-12-27 01:12:22 -0400

    cleaning

--- quickbooks/admin.py
+++ quickbooks/admin.py
@@ -3,9 +3,6 @@ from quickbooks.models import QWCTicket
 from quickbooks.models import UserProfile
 from quickbooks.models import ReceiveResponse
 
-
-
-
 admin.site.register(QWCTicket)
 admin.site.register(UserProfile)
 admin.site.register(ReceiveResponse)
