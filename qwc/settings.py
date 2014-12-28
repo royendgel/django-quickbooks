c03c189d482660c4b0db2bee87a3ac9371a81899
Royendgel Silberie <royendgel@gmail.com>
2014-12-28 19:00:14 -0400

    settings updated

--- qwc/settings.py
+++ qwc/settings.py
@@ -129,5 +129,12 @@ LOGGING = {
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+
+DATABASES = {'default': dj_database_url.parse('sqlite:///db.sqlite3')}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
