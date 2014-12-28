9f2359ecfe6985b04e2250845de9711bd3c2303b
Royendgel Silberie <royendgel@gmail.com>
2014-12-28 17:46:32 -0400

    updated

--- qwc/dev.py
+++ qwc/dev.py
@@ -1,2 +1,37 @@
 from qwc.settings import *
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'debug.log'),
+        },
+        'file_django': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_debug.log'),
+        },
+    },
+    'loggers': {
+        'quickbooks': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['file_django'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file_django'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 DEBUG = True
--- qwc/settings.py
+++ qwc/settings.py
@@ -104,40 +104,5 @@ LOGGING = {
     },
 }
 
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'debug.log'),
-        },
-        'file_django': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'django_debug.log'),
-        },
-    },
-    'loggers': {
-        'quickbooks': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django': {
-            'handlers': ['file_django'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['file_django'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
-
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
