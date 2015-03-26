5ad99715b763c9b72038e94c4c305082ab2b3b51
Royendgel Silberie <royendgel@gmail.com>
2015-03-26 04:14:39 -0400

    manifest

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-quickbooks',
-    version='0.2',
+    version='0.3',
     license='BSD License',  # example license
     description='Quickbooks integration with Django.',
     long_description=README,
