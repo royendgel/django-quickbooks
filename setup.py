568e64749c0392f2cbcbed9cd789133de24930a7
Royendgel Silberie <royendgel@gmail.com>
2015-09-03 08:00:08 -0400

    adding awesome changes of codedbyjay

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-quickbooks',
-    version='0.3',
+    version='0.4',
     license='BSD License',  # example license
     description='Quickbooks integration with Django.',
     long_description=README,
