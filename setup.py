96820699caf6326db6b33fddfa1bdc6a62134f2d
Royendgel Silberie <royendgel@gmail.com>
2015-09-05 10:39:35 -0400

    maxreturned to get_custoner

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-quickbooks',
-    version='0.4',
+    version='0.5',
     license='BSD License',  # example license
     description='Quickbooks integration with Django.',
     long_description=README,
